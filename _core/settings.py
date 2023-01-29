04eb0de130e65563c2e2d708cc8deaa06cfcbf5b
Leandro Schillreff <leandroschillreff@gmail.com>
2023-01-29 11:35:43 -0400

    style: removing comments

--- _core/settings.py
+++ _core/settings.py
@@ -167,8 +167,6 @@ STATICFILES_DIRS = (
     # '/var/www/static/',
 )
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR.joinpath('media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
